f230bd612c952cca326cf9e2b48b555bd1d8588f
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-10 09:43:39 -0300

    Create struct2.c

---
+++ Classes - Comparação/C/struct2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct {
+    char canto[20];
+    char traco[20];
+}Rect;
+
+void print (React *r){
+    printf("Retangulo possui o canto %s e seu estilo de contorno eh a %s\n", r->canto, r->traco);
+}
+
+int main(){
+    Rect r = {"arredondado", "opcao 3"};
+    print(&r);
+}

eb5ad3ca6fda9c5ff597c0a2b513350709cecf89
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-10 10:20:11 -0300

    Update struct2.c

--- Classes - Comparação/C/struct2.c
+++ Classes - Comparação/C/struct2.c
@@ -5,7 +5,7 @@ typedef struct {
     char traco[20];
 }Rect;
 
-void print (React *r){
+void print (Rect *r){
     printf("Retangulo possui o canto %s e seu estilo de contorno eh a %s\n", r->canto, r->traco);
 }
 

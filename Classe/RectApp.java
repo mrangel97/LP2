d220e6d4f4f57164281439a0482f5d8244fe7355
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-13 01:43:46 -0300

    Create RectApp.java

---
+++ Classe/RectApp.java
@@ -0,0 +1,18 @@
+public class RectApp {
+    public static void main (String[] args) {
+        Rect r1 = new Rect(50,25);
+		    int a = r1.area();
+        System.out.println(a);
+    }
+}
+class Rect {
+    int x, y;
+    Rect (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+	int area(){
+       return this.x * this.y;
+	}
+    
+}

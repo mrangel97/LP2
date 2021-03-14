56f8dbdb3003ca9b1f6a23b5b93b5378159abb72
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-14 13:38:38 -0300

    Create Rect2App.java

---
+++ Classe/Rect2App.java
@@ -0,0 +1,26 @@
+public class RectApp {
+    public static void main (String[] args) {
+        Rect r1 = new Rect(50,25);
+		    int a = r1.area();
+        System.out.println(a);
+		    r1.drag(10,20);
+    }
+}
+class Rect {
+    int x, y;
+    Rect (int x, int y) {
+        this.x = x;
+        this.y = y;
+	}
+	
+	int area(){
+       return this.x * this.y;
+	}
+	
+	void drag(int dx, int dy){
+		 dx += this.x;
+		 dy += this.y;
+		 System.out.format("dx = %d\ndy = %d\n", dx, dy);
+	}
+  
+}

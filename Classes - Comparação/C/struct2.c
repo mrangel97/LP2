ac5833d4a49d088bd5cbc21bbc15dc0ed5d50bad
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-15 09:48:36 -0300

    Update struct2.c

--- Classes - Comparação/C/struct2.c
+++ Classes - Comparação/C/struct2.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 typedef struct {
-    char canto[20];
-    char traco[20];
+    float x;
+    float y;
+    float w;
+    float h;
 }Rect;
 
 void print (Rect *r){
-    printf("Retangulo possui o canto %s e seu estilo de contorno eh a %s\n", r->canto, r->traco);
+    printf("O retangulo possui as coordenadas x e y iguais a %.2f e %.2f . E a largura e altura iguais a %.2f e %.2f", r->x, r->y, r->w, r->h);
 }
 
 int main(){
-    Rect r = {"arredondado", "opcao 3"};
+    Rect r = {62.4, 191.7, 79.9, 62.5};
     print(&r);
 }

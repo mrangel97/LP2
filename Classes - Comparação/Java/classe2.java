deb1073c34a963f8980ada397bedcb79b4096a46
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-15 09:53:03 -0300

    Update classe2.java

--- Classes - Comparação/Java/classe2.java
+++ Classes - Comparação/Java/classe2.java
@@ -1,18 +1,17 @@
 public class CircleApp {
      public static void main (String[] args){
-          Circle c = new Circle("opcao 2", "quadriculado");
+          Circle c = new Circle(27.5, 33.6);
           c.print();
      }
 }
 
 class Circle{
-     String traco;
-     String extremidade;
-     Circle(String traco, String extremidade){
-           this.traco = traco;
-           this.extremidade = extremidade;
+     double rx, ry;
+     Circle(double rx, double ry){
+           this.rx = rx;
+           this.ry = ry;
      }
      void print(){
-         System.out.format("Circulo possui o estilo de contorno a %s e sua extremidade eh %s", this.traco, this.extremidade);
+         System.out.format("O circulo tem a medida do raio vertical igual a %.2f e possui a medida do raio horizontal igual a %.2f", this.ry, this.rx);
      }
 }

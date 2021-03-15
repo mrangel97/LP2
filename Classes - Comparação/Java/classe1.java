3d4b59249503e48d0caf6c31dd4dc07fd320ef3d
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-15 09:51:46 -0300

    Update classe1.java

--- Classes - Comparação/Java/classe1.java
+++ Classes - Comparação/Java/classe1.java
@@ -1,14 +1,14 @@
 public class CircleApp {
      public static void main (String[] args){
-          Circle c = new Circle("opcao 2", "quadriculado");
+          Circle c = new Circle(27.5, 33.6);
+          c.print();
      }
 }
 
 class Circle{
-     String traco;
-     String extremidade;
-     Circle(String traco, String extremidade){
-           this.traco = traco;
-           this.extremidade = extremidade;
+     double rx, ry;
+     Circle(double rx, double ry){
+           this.rx = rx;
+           this.ry = ry;
      }
 }

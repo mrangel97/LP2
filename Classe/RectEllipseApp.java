28d01b1593c53b2c2c9f997491d42203b47e3894
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-20 00:12:56 -0300

    Create RectEllipseApp.java

---
+++ Classe/RectEllipseApp.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
+
+class RectEllipseApp {
+    public static void main (String[] args) {
+        RectEllipseFrame frame = new RectEllipseFrame();
+        frame.setVisible(true);
+    }
+}
+
+class RectEllipseFrame extends JFrame {
+    Rect r1;
+    Ellipse e1, e2, e3;
+
+    RectEllipseFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+        this.setTitle("Rect + Ellipse");
+        this.setSize(350, 350);
+        this.r1 = new Rect(50,50, 100,30);
+        this.e1 = new Ellipse(50,100, 100,30, Color.blue, Color.black);
+		    this.e2 = new Ellipse(50,200, 100,30, Color.yellow, Color.gray);
+		    this.e3 = new Ellipse(50,300, 100,30, Color.red, Color.pink);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        this.r1.paint(g);
+        this.e1.paint(g);
+		    this.e2.paint(g);
+		    this.e3.paint(g);
+    }
+}
+
+class Rect {
+    int x, y;
+    int w, h;
+
+    Rect (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    void paint (Graphics g) {
+        g.drawRect(this.x,this.y, this.w,this.h);
+    }
+}
+
+class Ellipse {
+    int x, y;
+    int w, h;
+	  Color cf, cc;
+
+    Ellipse (int x, int y, int w, int h, Color cf, Color cc) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+		    this.cf = cf;
+		    this.cc = cc;
+    }
+
+    void print () {
+        System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+		    g2d.setColor(this.cc);
+        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+		    g2d.setColor(this.cf);
+		    g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+    }
+}

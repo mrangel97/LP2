b5a1eeab12c201953e079e377bc3b8452d55afc2
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-20 12:06:13 -0300

    Update Line.java

--- Classe/Package/figures/Line.java
+++ Classe/Package/figures/Line.java
@@ -4,13 +4,13 @@ import java.awt.*;
 
 public class Line {
     private int x, y;
-	  private int w, h;
+    private int w, h;
 
     public Line (int x, int y, int w, int h) {
         this.x = x;
         this.y = y;
         this.w = w;
-		    this.h = h;
+	this.h = h;
     }
 
     private void print () {
@@ -22,7 +22,7 @@ public class Line {
         Graphics2D g2d = (Graphics2D) g;
         // através dessas linhas, é formado um triângulo
         g2d.drawLine(this.x, this.y, this.w, this.h);
-		    g2d.drawLine(this.x, this.y, this.w, this.h);
-		    g2d.drawLine(this.x, this.y, this.w, this.h);
+        g2d.drawLine(this.x, this.y, this.w, this.h);
+	g2d.drawLine(this.x, this.y, this.w, this.h);
     }
 }

6f562604477b93f8b57a9576b27a192612f02453
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-20 13:02:11 -0300

    Create Line.java

---
+++ Classe/Package - exercício 3.2/figures/Line.java
@@ -0,0 +1,32 @@
+package figures;
+
+import java.awt.*;
+
+public class Line {
+    private int x, y;
+	  private int w, h;
+	  private Color lc;
+
+    public Line (int x, int y, int w, int h, Color lc) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+		    this.h = h;
+		    this.lc = lc;
+    }
+
+    private void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+		    g2d.setColor(this.lc);
+        g2d.drawLine(this.x, this.y, this.w, this.h);
+		    g2d.setColor(this.lc);
+		    g2d.drawLine(this.x, this.y, this.w, this.h);
+		    g2d.setColor(this.lc);
+		    g2d.drawLine(this.x, this.y, this.w, this.h);
+    }
+}

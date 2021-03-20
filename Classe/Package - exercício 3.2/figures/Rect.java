29dc4e1ce91847e4331d30971f79fa29162d828e
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-20 12:59:44 -0300

    Update Rect.java

--- Classe/Package - exercício 3.2/figures/Rect.java
+++ Classe/Package - exercício 3.2/figures/Rect.java
@@ -5,15 +5,15 @@ import java.awt.*;
 public class Rect {
     int x, y;
     int w, h;
-	private Color rf, rc;
+    private Color rf, rc;
 
     public Rect (int x, int y, int w, int h, Color rf, Color rc) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-		this.rf = rf;
-		this.rc = rc;
+	this.rf = rf;
+	this.rc = rc;
 		
     }
 
@@ -24,9 +24,9 @@ public class Rect {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(this.rc);
+	g2d.setColor(this.rc);
         g2d.drawRect(this.x,this.y, this.w,this.h);
-		g2d.setColor(this.rf);
-		g2d.fillRect(this.x,this.y, this.w,this.h);
+	g2d.setColor(this.rf);
+	g2d.fillRect(this.x,this.y, this.w,this.h);
     }
 }

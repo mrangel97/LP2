fafb46665fe62e59eb61eb7ab21cbff693b41f12
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-21 23:07:48 -0300

    Update PackApp.java

--- Classe/Package/PackApp.java
+++ Classe/Package/PackApp.java
@@ -14,7 +14,7 @@ class PackApp {
 class PackFrame extends JFrame {
     Rect r1;
     Ellipse e1;
-    Line l1, l2, l3;
+    Triangle l1, l2, l3;
 
     PackFrame () {
         this.addWindowListener (
@@ -28,9 +28,9 @@ class PackFrame extends JFrame {
         this.setSize(350, 350);
         this.r1 = new Rect(50,50, 100,30);
         this.e1 = new Ellipse(50,200, 100,30);
-	this.l1 = new Line(120, 130, 280, 130);
-	this.l2 = new Line(120, 130, 200, 65);
-	this.l3 = new Line(200, 65, 280, 130);
+	this.l1 = new Triangle(120, 130, 280, 130);
+	this.l2 = new Triangle(120, 130, 200, 65);
+	this.l3 = new Triangle(200, 65, 280, 130);
     }
 
     public void paint (Graphics g) {

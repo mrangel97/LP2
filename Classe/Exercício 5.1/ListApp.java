c486b58e47eb566a7b9ee52245d4675a6e3bfd97
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-28 21:46:52 -0300

    Update ListApp.java

--- Classe/Exercício 5.1/ListApp.java
+++ Classe/Exercício 5.1/ListApp.java
@@ -15,7 +15,7 @@ class ListApp {
 
 class ListFrame extends JFrame {
     ArrayList<Rect> rs = new ArrayList<Rect>();
-	ArrayList<Ellipse> es = new ArrayList<Ellipse>();
+    ArrayList<Ellipse> es = new ArrayList<Ellipse>();
     Random rand = new Random();
 
     ListFrame () {
@@ -31,39 +31,39 @@ class ListFrame extends JFrame {
 		    new KeyAdapter() {
 			    public void keyPressed (KeyEvent evt) {
 				    if(evt.getKeyChar() == 'r') {
-					    int x = rand.nextInt(350);
-                        int y = rand.nextInt(350);
-                        int w = rand.nextInt(50);
-                        int h = rand.nextInt(50);
+					        int x = rand.nextInt(350);
+                                                int y = rand.nextInt(350);
+                                                int w = rand.nextInt(50);
+                                                int h = rand.nextInt(50);
 						int r = rand.nextInt(256);
 						int g = rand.nextInt(256);
 						int b = rand.nextInt(256);
 						int drawR = rand.nextInt(256);
 						int drawG = rand.nextInt(256);
 						int drawB = rand.nextInt(256);
-                        rs.add(new Rect(x, y, w, h, r, g, b, drawR, drawG, drawB));
+                                                rs.add(new Rect(x, y, w, h, r, g, b, drawR, drawG, drawB));
 						repaint();
-					}
-					else if(evt.getKeyChar() == 'e') {
-					    int x = rand.nextInt(350);
-                        int y = rand.nextInt(350);
-                        int w = rand.nextInt(50);
-                        int h = rand.nextInt(50);
+				     }
+				     else if(evt.getKeyChar() == 'e') {
+					        int x = rand.nextInt(350);
+                                                int y = rand.nextInt(350);
+                                                int w = rand.nextInt(50);
+                                                int h = rand.nextInt(50);
 						int r = rand.nextInt(256);
 						int g = rand.nextInt(256);
 						int b = rand.nextInt(256);
 						int drawR = rand.nextInt(256);
 						int drawG = rand.nextInt(256);
 						int drawB = rand.nextInt(256);
-                        es.add(new Ellipse(x, y, w, h, r, g, b, drawR, drawG, drawB));
+                                                es.add(new Ellipse(x, y, w, h, r, g, b, drawR, drawG, drawB));
 						repaint();
-					}	
-				}
+				     }	
+			      }
 			}
 		);
 
-        this.setTitle("Lista de Retangulos");
-        this.setSize(350, 350);
+                this.setTitle("Lista de Retangulos");
+                this.setSize(350, 350);
     }
 
     public void paint (Graphics g) {
@@ -71,8 +71,8 @@ class ListFrame extends JFrame {
         for (Rect r: this.rs) {
             r.paint(g);
         }
-		for (Ellipse e: this.es) {
-		    e.paint(g);
-		}
+	for (Ellipse e: this.es) {
+	    e.paint(g);
+	}
     }
 }

607eff1cd2ce2577f6f30184e7c6e0a6293cfa56
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-03-28 21:39:07 -0300

    Create Rect.java

---
+++ Classe/Exercício 5.1/figures/Rect.java
@@ -0,0 +1,37 @@
+package figures;
+
+import java.awt.*;
+
+public class Rect {
+    int x, y;
+    int w, h;
+	private int r, g, b;
+	private int drawR, drawG, drawB;
+
+    public Rect (int x, int y, int w, int h, int r, int g, int b, int drawR, int drawG, int drawB) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.drawR = drawR;
+		this.drawG = drawG;
+		this.drawB = drawB;
+		
+    }
+
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(new Color(this.r, this.g, this.b));
+		g2d.fillRect(this.x,this.y, this.w,this.h);
+		g2d.setColor(new Color(this.drawR, this.drawG, this.drawB));
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+    }
+}

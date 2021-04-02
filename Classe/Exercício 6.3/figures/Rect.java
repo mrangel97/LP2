75e5bfe48f71a00399f9e5fef009d2da2a6a5442
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-04-02 02:23:04 -0300

    Update Rect.java

--- Classe/Exercício 6.3/figures/Rect.java
+++ Classe/Exercício 6.3/figures/Rect.java
@@ -13,11 +13,11 @@ public class Rect extends Figure {
     }
 	
     public void print() {
-	       System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n", this.w, this.h, this.x, this.y);
+	     System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n", this.w, this.h, this.x, this.y);
     }
 	
     public void paint (Graphics g) {
 	      Graphics2D g2d = (Graphics2D) g;
-        g2d.drawRect(this.x, this.y, this.w, this.h);		
+              g2d.drawRect(this.x, this.y, this.w, this.h);		
     }
 }

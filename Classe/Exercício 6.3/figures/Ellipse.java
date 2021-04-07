3b6483fe66b0d4905841b24edbafb3436b98f73c
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-04-07 11:33:24 -0300

    Update Ellipse.java

--- Classe/Exercício 6.3/figures/Ellipse.java
+++ Classe/Exercício 6.3/figures/Ellipse.java
@@ -7,11 +7,17 @@ import java.awt.geom.Ellipse2D.Double;
 public class Ellipse extends Figure {
 	int w, h;
 	
-        public Ellipse (int x, int y, int w, int h) {
+        public Ellipse (int x, int y, int w, int h, int r, int g, int b, int drawR, int drawG, int drawB) {
 	        this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.drawR = drawR;
+		this.drawG = drawG;
+		this.drawB = drawB;
 	}
 	
 	public void print() {
@@ -20,6 +26,9 @@ public class Ellipse extends Figure {
 	
 	public void paint (Graphics g) {
 	    Graphics2D g2d = (Graphics2D) g;
+	    g2d.setColor(new Color(this.r, this.g, this.b));
+	    g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+	    g2d.setColor(new Color(this.drawR, this.drawG, this.drawB));
 	    g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
 	}
 }

d03b382babccd7985a04975cb0f2446cad756a2c
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-04-17 13:20:04 -0300

    Update Ellipse.java

--- Projeto/figures/Ellipse.java
+++ Projeto/figures/Ellipse.java
@@ -8,7 +8,7 @@ public class Ellipse extends Figure {
 	int w, h;
 	
     public Ellipse (int x, int y, int w, int h, int r, int g, int b, int drawR, int drawG, int drawB) {
-	    this.x = x;
+	        this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
@@ -26,9 +26,9 @@ public class Ellipse extends Figure {
 	
 	public void paint (Graphics g) {
 	    Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(new Color(this.r, this.g, this.b));
+	    g2d.setColor(new Color(this.r, this.g, this.b));
 	    g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
 	    g2d.setColor(new Color(this.drawR, this.drawG, this.drawB));
-		g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+	    g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
 	}
 }

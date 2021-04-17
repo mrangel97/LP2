df0a07d7ca93c0982b03073f92280815a39fd88c
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-04-17 13:18:23 -0300

    Update Arc.java

--- Projeto/figures/Arc.java
+++ Projeto/figures/Arc.java
@@ -6,7 +6,7 @@ public class Arc extends Figure {
 	int w, h, startAngle, arcAngle;
 	
 	public Arc (int x, int y, int w, int h, int startAngle, int arcAngle, int r, int g, int b, int drawR, int drawG, int drawB) {
-	    this.x = x;
+	        this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
@@ -25,10 +25,10 @@ public class Arc extends Figure {
 	}
 	
 	public void paint (Graphics g) {
-	    Graphics2D g2d = (Graphics2D) g;
+	        Graphics2D g2d = (Graphics2D) g;
 		g2d.setColor(new Color(this.r, this.g, this.b));
 		g2d.fillArc(this.x, this.y, this.w, this.h, this.startAngle, this.arcAngle);
 		g2d.setColor(new Color(this.drawR, this.drawG, this.drawB));
-        g2d.drawArc(this.x, this.y, this.w, this.h, this.startAngle, this.arcAngle);		
+                g2d.drawArc(this.x, this.y, this.w, this.h, this.startAngle, this.arcAngle);		
 	}
 }

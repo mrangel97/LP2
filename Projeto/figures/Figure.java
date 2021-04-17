bd9b9173e34be62058d8adedb6f24fe394b29f63
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-04-17 13:15:59 -0300

    Update Figure.java

--- Projeto/figures/Figure.java
+++ Projeto/figures/Figure.java
@@ -3,8 +3,8 @@ package figures;
 import java.awt.Graphics;
 
 public abstract class Figure {
-	int x, y;
-	int r, g, b;
+    int x, y;
+    int r, g, b;
     int drawR, drawG, drawB;
 	
     public abstract void paint (Graphics g);

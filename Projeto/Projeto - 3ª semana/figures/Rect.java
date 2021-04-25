7a39172e94374b3b3001396a3b5d38d413aa0518
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-04-25 04:31:52 -0300

    atualizado

--- Projeto/Projeto - 3ª semana/figures/Rect.java
+++ Projeto/Projeto - 3ª semana/figures/Rect.java
@@ -7,7 +7,7 @@ public class Rect extends Figure {
 	Rectangle rect;
 	
 	public Rect (int x, int y, int w, int h, int r, int g, int b, int drawR, int drawG, int drawB) {
-	    this.x = x;
+	        this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
@@ -26,17 +26,17 @@ public class Rect extends Figure {
 	
 	public boolean contains(MouseEvent evt){
 	    if(this.rect.contains(evt.getPoint())){
-			    return true; 
-		}else{
-		   return false;
-		}
+		return true; 
+	    }else{
+		return false;
+	    }
 	}
 	
 	public void paint (Graphics g) {
-	    Graphics2D g2d = (Graphics2D) g;
+	        Graphics2D g2d = (Graphics2D) g;
 		g2d.setColor(new Color(this.r, this.g, this.b));
 		g2d.fillRect(this.x,this.y, this.w,this.h);
 		g2d.setColor(new Color(this.drawR, this.drawG, this.drawB));
-        g2d.drawRect(this.x, this.y, this.w, this.h);		
+                g2d.drawRect(this.x, this.y, this.w, this.h);		
 	}
 }

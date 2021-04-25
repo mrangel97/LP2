c1a7380733473cdfbb6da9043b45d8f1448c690a
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-04-25 04:23:40 -0300

    atualizado

--- Projeto/Projeto - 3ª semana/figures/Figure.java
+++ Projeto/Projeto - 3ª semana/figures/Figure.java
@@ -4,34 +4,34 @@ import java.awt.*;
 import java.awt.event.*;
 
 public abstract class Figure {
-	public int x, y, w, h;
-	public int r, g, b;
+    public int x, y, w, h;
+    public int r, g, b;
     public int drawR, drawG, drawB;
 	
     public void zoom (int dw, int dh){
 	    this.w += dw;
-		this.h += dh;
+	    this.h += dh;
 		
-	}
+    }
 	
-	public void mover (int dx, int dy){
+    public void mover (int dx, int dy){
 	    this.x += dx;
-		this.y += dy;
+	    this.y += dy;
 		
-	}
+    }
 	
-	public void mudarCorContorno(int c1, int c2, int c3){
+    public void mudarCorContorno(int c1, int c2, int c3){
 	    this.drawR = c1;
-		this.drawG = c2;
-		this.drawB = c3;
-	}
+	    this.drawG = c2;
+	    this.drawB = c3;
+    }
 
     public void mudarCorFundo(int c1, int c2, int c3){
-        this.r = c1;
+                this.r = c1;
 		this.g = c2;
 		this.b = c3;
-	}	
+    }	
 	
-	public abstract void paint (Graphics g);
-	public abstract boolean contains(MouseEvent evt);
+    public abstract void paint (Graphics g);
+    public abstract boolean contains(MouseEvent evt);
 }

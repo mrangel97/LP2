46cdc0859134a4148aae3f6a9bbcf48defc0decf
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-05-09 15:51:02 -0300

    Update Exercicio.java

--- Interfaces/Exercício 2.1/Exercicio.java
+++ Interfaces/Exercício 2.1/Exercicio.java
@@ -5,14 +5,14 @@
 
 /* "Ligável":
         ligar()
-		desligar()
+	desligar()
 */
 
 // Sintaxe em Java
 
 interface Pluggable {
     void turnOn(int hour);    /* Nos dois métodos é usado o parâmetro hour para informar o horário que será ligado e desligado o(s) objeto(s) */
-	void turnOff(int hour);   
+    void turnOff(int hour);   
 }
 
 
@@ -23,12 +23,12 @@ interface Pluggable {
 
 /* "Marcável":
         marcar()
-		ler()
+	ler()
 */
 
 // Sintaxe em Java
 
 interface Markable {
     int mark(void);    // Nesse método é feito o retorno de um inteiro ao qual informa o número da página que o leitor parou de ler
-	void read(int pages);  // Nesse método é usado o parâmetro pages para informar a quantidade de páginas que o leitor irá ler 
+    void read(int pages);  // Nesse método é usado o parâmetro pages para informar a quantidade de páginas que o leitor irá ler 
 }

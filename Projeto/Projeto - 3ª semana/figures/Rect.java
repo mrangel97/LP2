14e04bc8923c422b51182a69b071d34ce9dc805f
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-05-12 23:16:56 -0300

    Update Rect.java

--- Projeto/Projeto - 3ª semana/figures/Rect.java
+++ Projeto/Projeto - 3ª semana/figures/Rect.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Rect extends Figure {
-	Rectangle rect;
 	
 	public Rect (int x, int y, int w, int h, int r, int g, int b, int drawR, int drawG, int drawB) {
 	        this.x = x;
@@ -17,19 +16,10 @@ public class Rect extends Figure {
 		this.drawR = drawR;
 		this.drawG = drawG;
 		this.drawB = drawB;
-		this.rect = new Rectangle(this.x, this.y, this.w, this.h);
 	}
 	
 	public void print() {
 	    System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n", this.w, this.h, this.x, this.y);
-	}
-	
-	public boolean contains(MouseEvent evt){
-	    if(this.rect.contains(evt.getPoint())){
-		return true; 
-	    }else{
-		return false;
-	    }
 	}
 	
 	public void paint (Graphics g) {

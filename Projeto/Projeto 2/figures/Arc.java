79c07cf4a8facbebcc0f6909f589ac786f2a16e4
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-05-13 02:29:11 -0300

    Update Arc.java

--- Projeto/Projeto 2/figures/Arc.java
+++ Projeto/Projeto 2/figures/Arc.java
@@ -9,7 +9,7 @@ public class Arc extends Figure {
 	int start, extent, type;
 	
 	public Arc (int x, int y, int w, int h, int start, int extent, int type, int r, int g, int b, int drawR, int drawG, int drawB) {
-	    this.x = x;
+	        this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
@@ -29,10 +29,10 @@ public class Arc extends Figure {
 	}
 	
 	public void paint (Graphics g) {
-	    Graphics2D g2d = (Graphics2D) g;
+	        Graphics2D g2d = (Graphics2D) g;
 		g2d.setColor(new Color(this.r, this.g, this.b));
 		g2d.fill(new Arc2D.Double(this.x, this.y, this.w, this.h, this.start, this.extent, this.type));
 		g2d.setColor(new Color(this.drawR, this.drawG, this.drawB));
-        g2d.draw(new Arc2D.Double(this.x, this.y, this.w, this.h, this.start, this.extent, this.type));		
+                g2d.draw(new Arc2D.Double(this.x, this.y, this.w, this.h, this.start, this.extent, this.type));		
 	}
 }

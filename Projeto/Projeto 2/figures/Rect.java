0bafa250627b8fd7c02c945c7768eb415b03e476
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-05-13 02:25:05 -0300

    Update Rect.java

--- Projeto/Projeto 2/figures/Rect.java
+++ Projeto/Projeto 2/figures/Rect.java
@@ -6,7 +6,7 @@ import java.awt.event.*;
 public class Rect extends Figure {
 	
 	public Rect (int x, int y, int w, int h, int r, int g, int b, int drawR, int drawG, int drawB) {
-	    this.x = x;
+	        this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
@@ -23,10 +23,10 @@ public class Rect extends Figure {
 	}
 	
 	public void paint (Graphics g) {
-	    Graphics2D g2d = (Graphics2D) g;
+	        Graphics2D g2d = (Graphics2D) g;
 		g2d.setColor(new Color(this.r, this.g, this.b));
 		g2d.fillRect(this.x,this.y, this.w,this.h);
 		g2d.setColor(new Color(this.drawR, this.drawG, this.drawB));
-        g2d.drawRect(this.x, this.y, this.w, this.h);		
+                g2d.drawRect(this.x, this.y, this.w, this.h);		
 	}
 }

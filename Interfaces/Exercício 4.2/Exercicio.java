740966c810690fdbf3b766752c9480b9eefdea79
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-05-23 18:29:04 -0300

    Update Exercicio.java

--- Interfaces/Exercício 4.2/Exercicio.java
+++ Interfaces/Exercício 4.2/Exercicio.java
@@ -2,31 +2,31 @@
 
    // a) Cenário representativo
    
-    /*         Classe D&D
+    /*              Classe D&D
 	          /      |     \
-			 /       |      \
-		    /        |       \
-		Guerreiro  Mago    Ladino
+	         /       |      \
+		/        |       \
+	     Guerreiro  Mago    Ladino
 		
 	*/
 	
 	/* b) Usei esse exemplo como caso de uso para a classe abstrata pois, ao construir um personagem de D&D, há um conjunto de classes fora 
-       esses que apresentei no cenário respresentativo que o jogador pode escolher para fazer o personagem. Com isso, foquei nisso como 
-	   uma superclasse e as classes dos personagens foquei como subclasses. E em cada classe, apresenta características em comum como
-       força, ataque, poder e outros. */
+              esses que apresentei no cenário respresentativo que o jogador pode escolher para fazer o personagem. Com isso, foquei nisso como 
+	      uma superclasse e as classes dos personagens foquei como subclasses. E em cada classe, apresenta características em comum como
+              força, ataque, poder e outros. */
 
-    // c) Esboço do código
+       // c) Esboço do código
 
-      public class abstract ClassD&D {
-            int force, attack;
-            String power;
+             public class abstract ClassD&D {
+                int force, attack;
+                String power;
 
-            public ClasseD&D(int force, int attack, String power){			
-	            this.force = force;
+                public ClasseD&D(int force, int attack, String power){			
+	                        this.force = force;
 				this.attack = attack;
 				this.power = power;
-			}
-      }
+		}
+             }
 
 
 // Interface
@@ -36,18 +36,18 @@
         // Objetos pensados: armário e um copo de requeijão
 
     /*   "Abrível":
-           abrir()
-           fechar()		   
+            abrir()
+            fechar()		   
         
-	*/
+    */
 
     /* b) Usei esse exemplo como caso de uso para a interface, pois os objetos pensados são de diferentes naturezas porém, são interagidos na 
           mesma forma para as pessoas pois esses métodos podem ser feitos com ambos objetos. Mas a forma de como vai ser feito os métodos abrir e 
-		  fechar vão ser diferentes pra cada objeto. */	
+          fechar vão ser diferentes pra cada objeto. */	
 		  
-	// c) Esboço do código 
+   // c) Esboço do código 
 	
 	    interface Openable {
 		    void open ( void );
-			void close( void );
+		    void close( void );
 		}

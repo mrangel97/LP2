44f52f07035e23d083bd63b646468fefece96578
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-05-23 23:54:47 -0300

    Update Figure.java

--- Projeto/Projeto 2/figures/Figure.java
+++ Projeto/Projeto 2/figures/Figure.java
@@ -2,41 +2,49 @@ package figures;
 
 import ivisible.IVisible;
 
-public abstract class Figure implements IVisible {
-    public int x, y, w, h;
-    public int r, g, b;
+public abstract class Figure implements IVisible{
+	public int x, y, w, h;
+	public int r, g, b;
     public int drawR, drawG, drawB;
+	
+	public Figure(int x, int y, int w, int h, int r, int g, int b, int drawR, int drawG, int drawB){
+	        this.x = x;
+			this.y = y;
+			this.w = w;
+			this.h = h;
+			this.r = r;
+			this.g = g;
+			this.b = b;
+			this.drawR = drawR;
+			this.drawG = drawG;
+			this.drawB = drawB;
+	}
 	
     public void zoom (int dw, int dh){
 	    this.w += dw;
-	    this.h += dh;
+		this.h += dh;
 		
-    }
+	}
 	
-    public void mover (int dx, int dy){
+	public void mover (int dx, int dy){
 	    this.x += dx;
-	    this.y += dy;
+		this.y += dy;
 		
-    }
+	}
 	
-    public void mudarCorContorno(int c1, int c2, int c3){
+	public void mudarCorContorno(int c1, int c2, int c3){
 	    this.drawR = c1;
-	    this.drawG = c2;
-	    this.drawB = c3;
-    }
+		this.drawG = c2;
+		this.drawB = c3;
+	}
 
     public void mudarCorFundo(int c1, int c2, int c3){
-                this.r = c1;
+        this.r = c1;
 		this.g = c2;
 		this.b = c3;
-    }
-
-   public boolean clicked (int x, int y) {
-	if((this.x <= x && x <= (this.w + this.x)) && (this.y <= y && y <= (this.y + this.h))){
-	     return true;
 	}
-	else{
-	     return false;
-	}	
-   }	
+
+   	public boolean clicked (int x, int y) {
+	    return((this.x <= x && x <= (this.w + this.x)) && (this.y <= y && y <= (this.y + this.h)));
+	}
 }

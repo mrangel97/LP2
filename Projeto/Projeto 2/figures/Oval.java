17a0b196ce0cf7eba39d79ad4682735968cde235
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-05-23 23:53:36 -0300

    Update Oval.java

--- Projeto/Projeto 2/figures/Oval.java
+++ Projeto/Projeto 2/figures/Oval.java
@@ -1,32 +1,28 @@
 package figures;
 
 import java.awt.*;
-import java.awt.event.*;
 
 public class Oval extends Figure {
 	
 	public Oval (int x, int y, int w, int h, int r, int g, int b, int drawR, int drawG, int drawB) {
-	        this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-		this.r = r;
-		this.g = g;
-		this.b = b;
-		this.drawR = drawR;
-		this.drawG = drawG;
-		this.drawB = drawB;
+	     super(x,y,w,h,r,g,b,drawR,drawG,drawB);
 	}
 	
 	public void print() {
 	    System.out.format("Oval de tamanho (%d,%d) na posicao (%d,%d).\n", this.w, this.h, this.x, this.y);
 	}
 	
-	public void paint (Graphics g) {
-	        Graphics2D g2d = (Graphics2D) g;
+	public void paint (Graphics g, boolean focused) {
+	    Graphics2D g2d = (Graphics2D) g;
+		
+		if(focused) {
+		   g2d.setColor(Color.red);
+		   g2d.drawOval(this.x-2, this.y-2, this.w+4, this.h+4);
+		}
+		
 		g2d.setColor(new Color(this.r, this.g, this.b));
 		g2d.fillOval(this.x,this.y, this.w,this.h);
 		g2d.setColor(new Color(this.drawR, this.drawG, this.drawB));
-                g2d.drawOval(this.x, this.y, this.w, this.h);		
+        g2d.drawOval(this.x, this.y, this.w, this.h);		
 	}
 }

59d679dcbcf3a4f3ef3416a9ccc1ca6c1a033420
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-05-28 01:58:34 -0300

    terminado

--- Projeto/Projeto 2/figures/Figure.java
+++ Projeto/Projeto 2/figures/Figure.java
@@ -5,10 +5,10 @@ import ivisible.IVisible;
 public abstract class Figure implements IVisible, Serializable{
 	public int x, y, w, h;
 	public int r, g, b;
-    public int drawR, drawG, drawB;
+        public int drawR, drawG, drawB;
 	
 	public Figure(int x, int y, int w, int h, int r, int g, int b, int drawR, int drawG, int drawB){
-	        this.x = x;
+	                this.x = x;
 			this.y = y;
 			this.w = w;
 			this.h = h;
@@ -20,26 +20,26 @@ public abstract class Figure implements IVisible, Serializable{
 			this.drawB = drawB;
 	}
 	
-    public void zoom (int dw, int dh){
-	    this.w += dw;
+        public void zoom (int dw, int dh){
+	        this.w += dw;
 		this.h += dh;
 		
 	}
 	
 	public void mover (int dx, int dy){
-	    this.x += dx;
+	        this.x += dx;
 		this.y += dy;
 		
 	}
 	
 	public void mudarCorContorno(int c1, int c2, int c3){
-	    this.drawR = c1;
+	        this.drawR = c1;
 		this.drawG = c2;
 		this.drawB = c3;
 	}
 
-    public void mudarCorFundo(int c1, int c2, int c3){
-        this.r = c1;
+        public void mudarCorFundo(int c1, int c2, int c3){
+                this.r = c1;
 		this.g = c2;
 		this.b = c3;
 	}

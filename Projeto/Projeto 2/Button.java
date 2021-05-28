5319bb02faa749a87d0e008e2b77541b0453491b
Marina Rangel <marina.anapurus.mr@gmail.com>
2021-05-28 02:03:20 -0300

    terminado

--- Projeto/Projeto 2/Button.java
+++ Projeto/Projeto 2/Button.java
@@ -3,16 +3,16 @@ import figures.Figure;
 import java.awt.*;
 
 public class Button implements IVisible {
-	    int idx;
+	        int idx;
 		private Figure fig;
 		
 		protected Button(int idx, Figure fig) {
 		   this.idx = idx;
 		   this.fig = fig;
 		   this.fig.x = 4+20;
-	       this.fig.y = 4+20+idx*40;
-	       this.fig.w = 40-4*2;
-	       this.fig.h = 40-4*2; 
+	           this.fig.y = 4+20+idx*40;
+	           this.fig.w = 40-4*2;
+	           this.fig.h = 40-4*2; 
 		}
         
 		public boolean clicked(int x, int y){
@@ -22,16 +22,16 @@ public class Button implements IVisible {
 		
 		public void paint(Graphics g, boolean focused) {
 			Graphics g2d = (Graphics2D) g;
-		    if(focused) {
-               g2d.setColor(Color.gray);
+		        if(focused) {
+                            g2d.setColor(Color.gray);
 			}
-            else {
-               g2d.setColor(Color.cyan);
+                        else {
+                            g2d.setColor(Color.cyan);
 			}
             
 			g2d.fillRect(20,20+this.idx*40,40,40);
 			g2d.setColor(Color.black);		
-            g2d.drawRect(20,20+this.idx*40,40,40);
-            this.fig.paint(g, false);			
+                        g2d.drawRect(20,20+this.idx*40,40,40);
+                        this.fig.paint(g, false);			
 		}
 }
